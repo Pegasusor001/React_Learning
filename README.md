b928e85aa0de13197ffb3d5cbbe479cd13743a48
Pegasusor001 <linus.ji.001@gmail.com>
2022-01-26 19:55:48 -0500

    add figure dic

--- README.md
+++ README.md
@@ -6,6 +6,10 @@
 - yarn start
 - Port number can be edited at package.json
 
+# Deploy App to Internet
+- yarn build - 部署到build文件夹
+- cd build，使用 Now 或者 Surge （两个常用网站）部署 static website
+
 # Key difference between HTML and JSX
 - JSX: className; HTML: class; 
 - JSX: Event Handller onClick; HTML: onclick; 
--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,9 @@
 import React, { useState, useEffect, useRef } from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import "./App.css";
+import "./css/App.css";
 
 import Props from "./components/Props/A_Props";
-import logo from "./logo.svg";
-import API from './components/API/A_API'
+import API from "./components/API/A_API";
 import Elements from "./components/Elements/A_Elements";
 import Hooks from "./components/Hooks/A_Hooks";
 import { RegExp } from "./components/Others/regExp";
@@ -12,7 +11,6 @@ import { RegExp } from "./components/Others/regExp";
 function App() {
   return (
     <div className="App">
-      <img src={logo} className="App-logo" alt="logo" />
       <Router>
         <Routes>
           <Route path="/props" element={<Props />} />
